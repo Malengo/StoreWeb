a6b1457d02f95e7b929175625cf2f936eba15531
Malengo <jose.malengo@hotmail.com>
2022-03-01 13:38:19 -0300

    remove imports

--- src/app/registerphone/register-phone.component.ts
+++ src/app/registerphone/register-phone.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Capacity } from '../models/entites/enum/capacity.enum';
-import { Memory } from '../models/entites/enum/Memory.enum';
-
-
-import { PhoneServiceService } from '../service/phone-service.service';
 
 @Component({
   selector: 'app-register-phone',
@@ -12,8 +6,7 @@ import { PhoneServiceService } from '../service/phone-service.service';
   styleUrls: ['./register-phone.component.css'],
 })
 export class RegisterPhoneComponent implements OnInit {
-
-buttonName: string;
+  buttonName: string;
   constructor() {
     this.buttonName = 'Cadastrar';
   }
