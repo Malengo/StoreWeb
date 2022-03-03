4056e2dfef7648615115ff8dd60f4e3de0fa0c3c
Malengo <jose.malengo@hotmail.com>
2022-03-03 14:28:53 -0300

    update project

---
+++ src/app/models/enum/Memory.enum.ts
@@ -0,0 +1,5 @@
+export enum Memory {
+  GB4 = "4gb",
+  GB6 = "6gb",
+  GB8 = "8gb"
+}
---
+++ src/app/models/enum/capacity.enum.ts
@@ -0,0 +1,6 @@
+export enum Capacity {
+  GB32 = "32gb",
+  GB64 = "64gb",
+  GB128 = "128gb",
+  GB256 = "256gb"
+}
