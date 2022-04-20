6c9c67b0998a8250ce7b8ccd2a380a96b6a3f629
Malengo <jose.malengo@hotmail.com>
2022-04-20 19:07:50 -0300

    add input date

--- src/app/models/entites/purchase.ts
+++ src/app/models/entites/purchase.ts
@@ -1,2 +1,9 @@
+import { Smartphone } from './smartphone';
 export class Purchase {
+
+    id? : number
+    dataPurchase? : Date
+    valuePurchase? : number
+    smartphoneId? : Smartphone
+
 }
--- src/app/purchase/purchase.module.ts
+++ src/app/purchase/purchase.module.ts
@@ -4,6 +4,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
 
 import { PurchaseRoutingModule } from './purchase-routing.module';
 import { PurchaseComponent } from './purchase.component';
@@ -18,6 +20,9 @@ import { PurchaseComponent } from './purchase.component';
     MatToolbarModule,
     MatFormFieldModule,
     MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRippleModule,
   ],
 })
 export class PurchaseModule {}
